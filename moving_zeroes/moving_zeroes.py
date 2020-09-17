0b7bb7d8de0b7f63e539da9729d35267370abb21
Daniel Mattox <dmattox10@gmail.com>
2020-09-17 14:17:40 -0400

    Solved moving zeros

--- moving_zeroes/moving_zeroes.py
+++ moving_zeroes/moving_zeroes.py
@@ -2,10 +2,21 @@
 Input: a List of integers
 Returns: a List of integers
 '''
-def moving_zeroes(arr):
-    # Your code here
 
-    pass
+
+def moving_zeroes(arr):
+    end = len(arr) - 1
+    for i in range(len(arr)):
+        if i >= end:
+            return arr
+        if arr[i] == 0:
+            while (arr[end] == 0) and (end > 1):
+                end -= 1
+            if i == end:
+                return arr
+            else:
+                arr[i], arr[end] = arr[end], arr[i]
+                end -= 1
 
 
 if __name__ == '__main__':

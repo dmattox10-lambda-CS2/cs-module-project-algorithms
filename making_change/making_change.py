63464d9bb48cfbce432402b429ee46e7d17f7756
Daniel Mattox <dmattox10@gmail.com>
2020-09-17 13:34:15 -0400

    Bad solution on counting coins

--- making_change/making_change.py
+++ making_change/making_change.py
@@ -2,18 +2,25 @@
 
 import sys
 
-def making_change(amount, denominations):
-  # Your code here
+# This is a very bad solution
 
-  pass
+
+def making_change(amount, denominations):
+    # Your code here
+    if amount < 0:
+        return 0
+    if amount == 0:
+        return 1
+    return making_change(amount - denominations[0], denominations) + making_change(amount - denominations[1], denominations) + making_change(amount - denominations[2], denominations) + making_change(amount - denominations[3], denominations) + making_change(amount - denominations[4], denominations)
 
 
 if __name__ == "__main__":
-  # Test our your implementation from the command line
-  # with `python making_change.py [amount]` with different amounts
-  if len(sys.argv) > 1:
-    denominations = [1, 5, 10, 25, 50]
-    amount = int(sys.argv[1])
-    print("There are {ways} ways to make {amount} cents.".format(ways=making_change(amount, denominations), amount=amount))
-  else:
-    print("Usage: making_change.py [amount]")
+    # Test our your implementation from the command line
+    # with `python making_change.py [amount]` with different amounts
+    if len(sys.argv) > 1:
+        denominations = [1, 5, 10, 25, 50]
+        amount = int(sys.argv[1])
+        print("There are {ways} ways to make {amount} cents.".format(
+            ways=making_change(amount, denominations), amount=amount))
+    else:
+        print("Usage: making_change.py [amount]")
